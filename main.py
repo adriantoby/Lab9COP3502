24d9494ea522952704e9828359ab810e9e1175db
Adrian <moreno.a@ufl.edu>
2024-04-10 16:38:12 -0400

    Added the looping logic and encode functionality

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from encode import encode
+
 def main():
-  pass
+  option = ""
+  while True:
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit\n")
+    option = input("Please enter an option: ")
+
+    if option == "1":
+      password = input("Please enter your password to encode: ")
+      password = encode(password)
+      print("Your password has been encoded and stored!\n")
+    elif option == "2":
+      pass
+    elif option == "3":
+      break
+
 
 if __name__ == "__main__":
   main()
